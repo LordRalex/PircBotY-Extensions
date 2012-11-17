537e06b34e6c94eab7ee4604027f6d21f7dd6c27
Joshua <Joshua@localhost.localdomain>
2012-11-17 00:58:11 -0600

    Add multi-config support

--- RalexBot/src/com/lordralex/ralexbot/settings/Settings.java
+++ RalexBot/src/com/lordralex/ralexbot/settings/Settings.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,14 +19,16 @@ import org.yaml.snakeyaml.constructor.SafeConstructor;
 
 public final class Settings {
 
-    private static final Map<String, Object> settings = new ConcurrentHashMap<>();
+    private static final Map<File, Map<String, Object>> settings = new ConcurrentHashMap<>();
+    private File name;
 
-    public static void loadSettings() {
-        File settingsFile = new File("settings", "config.yml");
+    public Settings(File aFileToLoad) {
+        name = aFileToLoad;
+        Map<String, Object> local = new HashMap<>();
         Yaml yml = new Yaml(new SafeConstructor());
         Iterable<Object> it = null;
         try {
-            it = yml.loadAll(new FileInputStream(settingsFile));
+            it = yml.loadAll(new FileInputStream(name));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -35,43 +38,49 @@ public final class Settings {
                 //currently does not do nested vars, only outer, have to loop for LinkedHashMaps for that
                 for (Object key : set) {
                     if (((LinkedHashMap) in).get(key) != null) {
-                        settings.put((String) key, ((LinkedHashMap) in).get(key));
+                        local.put((String) key, ((LinkedHashMap) in).get(key));
                     }
                 }
             }
         }
+        settings.put(name, local);
     }
 
-    public static String getString(String key) {
+    public static Settings loadGlobalSettings() {
+        File settingsFile = new File("settings", "config.yml");
+        return new Settings(settingsFile);
+    }
+
+    public String getString(String key) {
         if (key == null || key.isEmpty()) {
             throw new NullPointerException("KEY CANNOT BE NULL");
         }
         String value = null;
-        Object val = settings.get(key);
+        Object val = settings.get(name).get(key);
         if (val instanceof String) {
             value = (String) val;
         }
         return value;
     }
 
-    public static int getInt(String key) {
+    public int getInt(String key) {
         if (key == null || key.isEmpty()) {
             throw new NullPointerException("KEY CANNOT BE NULL");
         }
         Integer value = 0;
-        Object val = settings.get(key);
+        Object val = settings.get(name).get(key);
         if (val instanceof Integer) {
             value = (Integer) val;
         }
         return value;
     }
 
-    public static List<String> getStringList(String key) {
+    public List<String> getStringList(String key) {
         if (key == null || key.isEmpty()) {
             throw new NullPointerException("KEY CANNOT BE NULL");
         }
         List<String> value = null;
-        Object val = settings.get(key);
+        Object val = settings.get(name).get(key);
         if (val instanceof List) {
             value = (List<String>) val;
         } else if (val instanceof String[]) {
@@ -86,16 +95,13 @@ public final class Settings {
      * @param key Key
      * @param newValue The new value, this can be lists
      */
-    public static void set(String key, Object newValue) throws IOException {
+    public void set(String key, Object newValue) throws IOException {
         if (key == null || key.isEmpty()) {
             throw new NullPointerException("KEY CANNOT BE NULL");
         }
         Yaml yml = new Yaml(new SafeConstructor());
-        settings.put(key, newValue);
-        FileWriter out = new FileWriter(new File("settings", "config.yml"));
-        yml.dump(settings, out);
-    }
-
-    private Settings() {
+        settings.get(name).put(key, newValue);
+        FileWriter out = new FileWriter(name);
+        yml.dump(settings.get(name), out);
     }
 }
