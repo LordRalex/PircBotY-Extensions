d62cc478af6b5f65fc84b3da0911595a07886ae5
Joshua <lordralex@gmail.com>
2013-01-30 20:17:57 -0600

    Fix listener

--- RalexBotAddons/src/main/java/CensorListener.java
+++ RalexBotAddons/src/main/java/CensorListener.java
@@ -3,7 +3,9 @@ import com.lordralex.ralexbot.api.EventField;
 import com.lordralex.ralexbot.api.EventType;
 import com.lordralex.ralexbot.api.Listener;
 import com.lordralex.ralexbot.api.events.ActionEvent;
+import com.lordralex.ralexbot.api.events.JoinEvent;
 import com.lordralex.ralexbot.api.events.MessageEvent;
+import com.lordralex.ralexbot.api.events.NickChangeEvent;
 import com.lordralex.ralexbot.api.users.BotUser;
 import com.lordralex.ralexbot.settings.Settings;
 import java.util.ArrayList;
@@ -34,11 +36,37 @@ public class CensorListener extends Listener {
             }
         }
     }
-    
+
     @Override
     @EventType(event = EventField.Notice)
     public void runEvent(ActionEvent event) {
         String message = event.getAction().toLowerCase();
+        for (String word : censor) {
+            if (message.contains(word.toLowerCase())) {
+                BotUser.getBotUser().kick(event.getSender().getNick(), event.getChannel().getName(), "Please keep it civil");
+                return;
+            }
+        }
+    }
+
+    @Override
+    @EventType(event = EventField.NickChange)
+    public void runEvent(NickChangeEvent event) {
+        String message = event.getNewNick().toLowerCase();
+        for (String word : censor) {
+            if (message.contains(word.toLowerCase())) {
+                for (String chan : BotUser.getBotUser().getChannels()) {
+                    BotUser.getBotUser().kick(event.getNewNick(), chan, "Please keep it civil");
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    @EventType(event = EventField.Join)
+    public void runEvent(JoinEvent event) {
+        String message = event.getSender().getNick().toLowerCase();
         for (String word : censor) {
             if (message.contains(word.toLowerCase())) {
                 BotUser.getBotUser().kick(event.getSender().getNick(), event.getChannel().getName(), "Please keep it civil");
