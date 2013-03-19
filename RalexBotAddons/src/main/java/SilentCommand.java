8e52ede280901ba759ec4fac3fe924e2fdd0d957
LordRalex <lordralex@gmail.com>
2013-03-19 13:26:27 -0500

    Add licences to addons

--- RalexBotAddons/src/main/java/SilentCommand.java
+++ RalexBotAddons/src/main/java/SilentCommand.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 Lord_Ralex
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import com.lordralex.ralexbot.api.EventField;
 import com.lordralex.ralexbot.api.EventType;
@@ -66,8 +82,8 @@ public class SilentCommand extends Listener {
     @Override
     public String[] getAliases() {
         return new String[]{
-                    "silent",
-                    "unsilent"
-                };
+            "silent",
+            "unsilent"
+        };
     }
 }
