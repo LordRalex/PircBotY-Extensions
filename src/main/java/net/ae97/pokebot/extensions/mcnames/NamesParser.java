0787e6d2cc44f8663873739c7113dd8a857d2197
Uriel Salischiker <uriel@urielsalis.me>
2017-01-26 16:46:17 -0300

    search

--- src/main/java/net/ae97/pokebot/extensions/mcnames/NamesParser.java
+++ src/main/java/net/ae97/pokebot/extensions/mcnames/NamesParser.java
@@ -1,7 +1,23 @@
 package net.ae97.pokebot.extensions.mcnames;
 
+import net.ae97.pokebot.PokeBot;
+import net.ae97.pokebot.extension.Extension;
+import net.ae97.pokebot.extensions.hjt.HJTListener;
+
 /**
  * Created by urielsalis on 1/26/2017
  */
-public class NamesParser {
+public class NamesParser extends Extension{
+    @Override
+    public String getName() {
+        return "DxdiagParser";
+    }
+
+    @Override
+    public void load() {
+        NamesListener listener = new NamesListener(this);
+        PokeBot.getEventHandler().registerListener(listener);
+        PokeBot.getEventHandler().registerCommandExecutor(listener);
+
+    }
 }
