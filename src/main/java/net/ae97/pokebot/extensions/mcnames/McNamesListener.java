2fbd18dc94d760bc188b92c3520e8848f673ae94
Michael Ripley <zkxs00@gmail.com>
2017-01-30 07:09:01 -0600

    Bracketize if statements

--- src/main/java/net/ae97/pokebot/extensions/mcnames/McNamesListener.java
+++ src/main/java/net/ae97/pokebot/extensions/mcnames/McNamesListener.java
@@ -44,7 +44,9 @@ public class McNamesListener implements Listener, CommandExecutor {
     }
 
     private String getNS(String s, boolean extended) {
-        if (s == null || s.isEmpty()) return "Invalid username";
+        if (s == null || s.isEmpty()) {
+            return "Invalid username";
+        }
         long unixTimestamp = System.currentTimeMillis() / 1000l;
 
 
@@ -53,8 +55,9 @@ public class McNamesListener implements Listener, CommandExecutor {
             result = findInfo(s, extended, true, unixTimestamp - 2505600); //2500 seems to always work, get previous name
             if (result == null || result.isEmpty()) {
                 result = findInfo(s, extended, true, 0); //get original name
-                if (result == null || result.isEmpty())
-                    return ChatFormat.RED + "Username doesn't exist" + ChatFormat.NORMAL;
+                if (result == null || result.isEmpty()){
+                    return ChatFormat.RED + "Username doesn't exist" + ChatFormat.NORMAL;                    
+                }
             }
         }
         return result;
@@ -63,7 +66,9 @@ public class McNamesListener implements Listener, CommandExecutor {
     private String findInfo(String s, boolean extended, boolean previousName, long timestamp) {
         try {
             String str = "https://api.mojang.com/users/profiles/minecraft/" + s;
-            if (previousName) str = str + "?at=" + timestamp;
+            if (previousName) {
+                str = str + "?at=" + timestamp;
+            }
             URL url = new URL(str);
             HttpURLConnection request = (HttpURLConnection) url.openConnection();
             request.connect();
@@ -76,11 +81,17 @@ public class McNamesListener implements Listener, CommandExecutor {
             String currentName = rootobj.get("name").getAsString();
             boolean migrated = true;
             boolean paid = true;
-            if (rootobj.has("legacy")) migrated = false;
-            if (rootobj.has("demo")) paid = false;
+            if (rootobj.has("legacy")) {
+                migrated = false;
+            }
+            if (rootobj.has("demo")) {
+                paid = false;
+            }
             String[] names = getNames(id, extended);
             StringBuilder output = new StringBuilder();
-            if (previousName) output.append(ChatFormat.RED + "Name was changed to: " + ChatFormat.NORMAL);
+            if (previousName) {
+                output.append(ChatFormat.RED + "Name was changed to: " + ChatFormat.NORMAL);
+            }
             output.append(ChatFormat.BOLD + currentName + ChatFormat.NORMAL + ": " + ChatFormat.BLUE + "UUID: " + ChatFormat.NORMAL + id + " ");
             output.append(paid ? ChatFormat.GREEN + "PAID " + ChatFormat.NORMAL : ChatFormat.RED + "DEMO " + ChatFormat.NORMAL);
             output.append(migrated ? ChatFormat.YELLOW + "MIGRATED " + ChatFormat.NORMAL : ChatFormat.RED + "LEGACY " + ChatFormat.NORMAL);
@@ -105,7 +116,9 @@ public class McNamesListener implements Listener, CommandExecutor {
             JsonParser jp = new JsonParser(); //from gson
             JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
             JsonArray rootobj = root.getAsJsonArray(); //May be an array, may be an object.
-            if (rootobj.size() == 1) return new String[]{};
+            if (rootobj.size() == 1) {
+                return new String[]{};
+            }
             SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
             if (extended) {
                 String[] names = new String[rootobj.size() - 1];
@@ -119,7 +132,9 @@ public class McNamesListener implements Listener, CommandExecutor {
                 return names;
             } else {
                 int limit = rootobj.size() - 3;
-                if (limit < 0) limit = 0;
+                if (limit < 0) {
+                    limit = 0;
+                }
                 String[] names = new String[rootobj.size() - limit];
                 int counter = 0;
                 for (int i = rootobj.size() - 1; i >= limit; i--) {
