f67386b2a6c7e20081ad3a4d2abcc731b369b2d7
Michael Ripley <zkxs00@gmail.com>
2017-01-30 13:53:45 -0600

    Remove unused import

--- src/main/java/net/ae97/pokebot/extensions/mcnames/McNamesExtension.java
+++ src/main/java/net/ae97/pokebot/extensions/mcnames/McNamesExtension.java
@@ -1,7 +1,6 @@
 package net.ae97.pokebot.extensions.mcnames;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
