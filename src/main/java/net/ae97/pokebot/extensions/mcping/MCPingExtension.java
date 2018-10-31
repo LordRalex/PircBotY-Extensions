83b60521ef7a194bb8d514b0a0a6fc680ff53e41
urielsalis <uriel.salischiker@n26.com>
2018-10-31 17:51:55 +0100

    Check blacklist of servers in mcping(doesnt check ip addresses!)

--- src/main/java/net/ae97/pokebot/extensions/mcping/MCPingExtension.java
+++ src/main/java/net/ae97/pokebot/extensions/mcping/MCPingExtension.java
@@ -19,6 +19,19 @@ package net.ae97.pokebot.extensions.mcping;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Formatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import net.ae97.pircboty.api.events.CommandEvent;
 import net.ae97.pokebot.PokeBot;
 import net.ae97.pokebot.api.CommandExecutor;
@@ -41,7 +54,23 @@ public class MCPingExtension extends Extension implements CommandExecutor {
         if (ce.getArgs().length != 1) {
             ce.respond("Usage: mcping <server ip>[:port]");
             return;
-        }       
+        }
+        try {
+            HttpURLConnection request = (HttpURLConnection) new URL("https://sessionserver.mojang.com/blockedservers")
+                    .openConnection();
+            request.connect();
+            final List<String> blacklisted = new BufferedReader(new InputStreamReader(request.getInputStream()))
+                    .lines()
+                    .collect(Collectors.toList());
+            final String url = ce.getArgs()[0].split(":")[0];
+            boolean isBlacklisted = isBlacklisted(blacklisted, url);
+            if(isBlacklisted) {
+                ce.respond("Server is blacklisted");
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         try {
             Process pinger = new ProcessBuilder().command("python", "bin/mcping.py", ce.getArgs()[0]).start();
             pinger.waitFor();
@@ -63,5 +92,49 @@ public class MCPingExtension extends Extension implements CommandExecutor {
     @Override
     public String getName() {
         return "mcping";
+    }
+
+    private static String getHash(String url)
+    {
+        String sha1 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(url.getBytes(StandardCharsets.UTF_8));
+            sha1 = byteToHex(crypt.digest());
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    private static String byteToHex(final byte[] hash)
+    {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+
+    private boolean isBlacklisted(List<String> blacklisted, String url) {
+        final List<String> parts = Arrays.asList(url.split("\\."));
+        if(blacklisted.contains(getHash(url))) {
+            return true;
+        }
+        for (int i = 1; i < parts.size(); i++) {
+            final String asterisks = String.join("", Collections.nCopies(i, "*."));
+            final String finalUrl = asterisks + String.join(".", parts.subList(i, parts.size()));
+            if(blacklisted.contains(getHash(finalUrl))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
